09fff2517ccfc491dd75860ff6a7a7e296e6ec28
Harsh Jani <harshjani53@gmail.com>
2023-09-28 20:45:54 +0530

    first commit

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from distutils.core import setup
 setup(
   name = 'jani_text_cleaning',
   packages = ['jani_text_cleaning'],
-  version = '0.11',
+  package_dir={'jani_text_cleaning': 'src/mypkg'},
+  package_data={'jani_text_cleaning': ['cleaning/*.py']},
+  version = 'v0.0.1',
   license='MIT',
   description = 'This python package does cleaning tasks on textual data and gives tokenized output.',
   author = 'HARSH JANI',
   author_email = 'harshjani53@gmail.com',
   url = 'https://github.com/harshjani53/jani_text_cleaning',
-  download_url = 'https://github.com/harshjani53/jani_text_cleaning/archive/refs/tags/0.1.1.tar.gz',   
+  download_url = 'https://github.com/harshjani53/jani_text_cleaning/archive/refs/tags/v0.0.1.tar.gz',   
   install_requires=[
         'pandas','openpyxl','numpy','nltk'
       ])
