2b223a99b820538011e7e1b5d6d4c1ecfda98894
Jani Harsh <82042071+harshjani53@users.noreply.github.com>
2023-09-29 22:19:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
   packages = ['jani_text_cleaning'],
   package_dir={'jani_text_cleaning': 'jani_text_cleaning'},
   package_data={'jani_text_cleaning': ['cleaning/*.py']},
-  version = '1.1',
+  version = '1.2',
   license='MIT',
   description = 'This python package does cleaning tasks on textual data and gives tokenized output.',
   author = 'HARSH JANI',
   author_email = 'harshjani53@gmail.com',
   url = 'https://github.com/harshjani53/jani_text_cleaning',
-  download_url = 'https://github.com/harshjani53/jani_text_cleaning/archive/refs/tags/1.1.tar.gz',   
+  download_url = 'https://github.com/harshjani53/jani_text_cleaning/archive/refs/tags/1.2.tar.gz',   
   install_requires=[
         'pandas','openpyxl','numpy','nltk'
       ])
